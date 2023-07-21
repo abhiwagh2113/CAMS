dba7f04dd59f04042e67e3794bf9f9e0e19b8ef6
Abhishek Wagh <abhishekwagh2113@gmail.com>
2023-07-21 19:58:32 +0530

    cams

---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+include_once('user/includes/dbconnection.php');
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<meta name="viewport" content="width=device-width, intial-scale=1.0">
+<title>Abhishek Wagh college</title>
+<link rel="stylesheet" href="css/style.css"/>
+<!--fav-icon-->
+<link rel="shortcut icon" href="images/favicon.png"/>
+
+</head>
+
+<body>
+    
+    <section class="main" style="background-image: url(images/hero-bg.png); height:100vh;">
+        
+        <nav>
+            <a href="#" class="logo">
+                <img src="images/loo.png" class="image1"width="320px" height="100px" />
+            </a>
+            <input class="menu-btn" type="checkbox" id="menu-btn"/>
+            <label class="menu-icon" for="menu-btn">
+                <span class="nav-icon"></span>
+            </label>
+            <ul class="menu" style="border-radius: 5px;">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Notification</a></li>
+                <li><a href="#">Results</a></li>
+                <li><a href="#courses">Courses</a></li>
+                <li><a class="active" href="user/signup.php" onclick="document.getElementById('id01').style.display='block'" style="width:auto; border-radius: 5px; cursor: pointer;">Sign Up</a></li>
+                <li><a class="active" href="user/login.php" onclick="document.getElementById('id01').style.display='block'" style="width:auto; border-radius: 5px; cursor: pointer;">sign in</a></li>
+            </ul>
+        </nav>
+       
+    
+
+
+        <!--main-content-->
+        <div class="home-content">
+            
+            <!--text-->
+            <div class="home-text" >
+                
+                <h3 style="color: white; letter-spacing: 3px;">Welcome to Abhishek Wagh College</h3>
+                <h1 style="color: white;"> Student Portal</h1>
+                <p style="color: white;">Abhishek Wagh college provides a student centre of learning environment with close student faculty interaction and constant participation. The Principal mission of our college is to achieve success.</p>
+            <!--login-btn-->
+            <a href="user/signup.php" class="main-login" style="border-radius: 5px;">Apply Now</a>
+            </div>
+            <!--img-->
+            <div class="home-img" style="width: 500px;">
+                <img src="images/hero.png" width="500px" style="text-shadow: 20px 22px;"/>
+               
+                     
+
+            </div>
+            
+        </div>
+        
+        
+    </section>
+   <!--LATEST NOTIFICATION START -->
+     <section class="notification">
+
+                         <hr>
+<marquee behavior="alternate" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();">
+    <?php $query=mysqli_query($con,"select * from tblnotice");
+while ($row=mysqli_fetch_array($query)) {
+?>
+
+    <a href="notice-details.php?nid=<?php echo $row['ID'];?>" target="_blank"><?php echo $row['Title'];?> &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+    
+    <?php } ?>
+    
+    
+</marquee>
+
+        
+    </section>
+    <hr>
+    <!--LATEST NOTIFICATION END-->
+    <!--services----------------------->
+    <section class="services" id="courses">
+        <!--heading----------->
+        <div class="services-heading">
+            <h2>OUR PROFESSIONAL COURSES</h2>
+            <p>Some of the Best and Popular Courses from our College.</p>
+        </div>
+        <!--box-container----------------->
+        <div class="box-container">
+            <!--box-1-------->
+            <div class="box">
+                <img src="images/icon5.png">
+                <font>Batchlor of Computer Application</font>
+                <p>BCA is a undergraduate Degree that focuses on knowledge of computer application and software development.</p>
+                <!--btn--------->
+                <a href="user/signup.php">Apply Now</a>
+            </div>
+            <!--box-2-------->
+            <div class="box">
+                <img src="images/icon5.png">
+                <font>Batchlor of Business Administration</font>
+                <p>A Business Administration Bachelor's is also a great first step towards a future MBA. </p>
+                <!--btn-------->
+                <a href="user/signup.php">Apply Now</a>
+            </div>
+            <!--box-3-------->
+            <div class="box">
+                <img src="images/icon5.png">
+                <font>Bio-Technology</font>
+                <p>Biotechnology course is  a combination of science subjects and cover topics such as recombinant DNA technology.</p>
+                <!--btn--------->
+                <a href="user/signup.php">Apply Now</a>
+            </div>
+            <!--box-4-------->
+            <div class="box">
+                <img src="images/icon5.png">
+                <font>Computer Science</font>
+                <p>the study of computers and computing,including their theoretical and algorithmic foundations, hardware and software .</p>
+                <!--btn--------->
+                <a href="user/signup.php">Apply Now</a>
+            </div>
+            <!--box-1-------->
+            
+        </div>
+    </section>
+    <footer>
+        <p>Abhishek Wagh Collge</p>
+    </footer>
+   
+</body>
+
+</html>
